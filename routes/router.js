72c4e0e82b8782056a45263d1ef832e87d85878a
MimoBram <bimoadi.bramantyo@gmail.com>
2026-05-28 14:02:23 +0700

    Create Views Folders & Files, Create Controllers Folder & Files, Setup Package on Router & Fixing app.js

--- routes/router.js
+++ routes/router.js
@@ -1,3 +1,9 @@
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const { User, Animal, Farm, Order, OrderItem, UserProfile } = require('../models');
+const bcrypt = require('bcryptjs');
+const { USE } = require('sequelize/lib/index-hints');
+
+// Routing Starts Here
 
 module.exports = router;
